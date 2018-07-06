7bd77bd487aa67f3420aeb7462a518ca3a263bed
Cas <casildaruth@gmail.com>
2018-07-06 10:35:41 -0500

    (UPDATED) The hangman spec.

--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -1,13 +1,23 @@
 require "hangman"
 
 RSpec.describe HangMan, "HangMan" do 
-    let(:new_game) { HangMan.new }
+    describe "get_random_game_word" do
+        let(:new_game) { HangMan.new }
 
         it "only selects from random_words" do
-        new_game.get_random_game_word do |char|
-            expect(["light", "dark", "neutral", "moon", "utopia", "george", "bobby"].include?(char)).to eq(true)
+            new_game.get_random_game_word do |char|
+                expect(["light", "dark", "neutral", "moon", "utopia", "george", "bobby"].include?(char)).to eq(true)
+            end
         end
     end
+    describe "create_blanks" do 
+        it "should create a new array" do
+            expect(Array.new).to eq([])
+        end
+    end
+
 end
+
+
 
 
